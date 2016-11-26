fda876c4a3e623c01520cb83de578ea72d8ca18b
longweifeng <longweifeng@outlook.com>
2016-11-26 13:41:15 +0800

    相机  相册功能模块初步建成

--- Project_shucai/app/src/main/java/com/example/along/ui1project/PersonalInfo.java
+++ Project_shucai/app/src/main/java/com/example/along/ui1project/PersonalInfo.java
@@ -3,6 +3,7 @@ package com.example.along.ui1project;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -13,6 +14,7 @@ import android.os.health.PackageHealthStats;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,12 +39,16 @@ public class PersonalInfo extends Activity {
             user_sex,//用户性别
             user_sex_next_arrow,//性别后的箭头
             personalized_edit_signature,//个人签名
-            personalized_edit_signature_next_arrow;//个人签名后的箭头
+            personalized_edit_signature_next_arrow,//个人签名后的箭头
+            camera,//相机
+            fromPhotoAlbum;//相册
+
+
     ImageView info_head_portrait;//用户头像
     LayoutInflater layoutInflater;//转换器
-    AlertDialog cameraDialog;//更换头像弹
+    PopupWindow cameraWindow;//更换头像弹
     //更换用户头像
-    LinearLayout info_head_change,
+    LinearLayout info_head_change, parent_panel,
             sexual;
 
 
@@ -56,13 +62,13 @@ public class PersonalInfo extends Activity {
         head_portrait_next_arrow = (TextView) findViewById(R.id.head_portrait_next_arrow);//头像箭头
         nick_name_next_arrow = (TextView) findViewById(R.id.nick_name_next_arrow);//昵称后的箭头
         user_sex = (TextView) findViewById(R.id.user_sex);//用户性别
-        sexual= (LinearLayout) findViewById(R.id.sexual);
+        sexual = (LinearLayout) findViewById(R.id.sexual);
         user_sex_next_arrow = (TextView) findViewById(R.id.user_sex_next_arrow);//性别后的箭头
         personalized_edit_signature = (TextView) findViewById(R.id.personalized_edit_signature);//个人签名
         personalized_edit_signature_next_arrow = (TextView) findViewById(R.id.personalized_edit_signature_next_arrow);//个人签名后的箭头
         info_head_portrait = (ImageView) findViewById(R.id.info_head_portrait);//用户头像
         info_head_change = (LinearLayout) findViewById(R.id.info_head_change);//更换用户头像
-
+        parent_panel = (LinearLayout) findViewById(R.id.parent_panel);
         info_head_change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,59 +83,57 @@ public class PersonalInfo extends Activity {
         });
     }
 
-    //设置屏幕背景透明度
-    public void backGroundAlpha(float alpha) {
-        WindowManager.LayoutParams l = this.getWindow().getAttributes();
-        l.alpha = alpha;
-        getWindow().setAttributes(l);
-    }
 
     //创建选择头像弹出框
     public void setHeadPortraitWindow() {
         View view = layoutInflater.inflate(R.layout.chang_head_protrait, null);
-        cameraDialog = new AlertDialog.Builder(PersonalInfo.this).create();
-        TextView camera = (TextView) view.findViewById(R.id.head_camera);//拍照获取头像
-        TextView headChangeCancel = (TextView) view.findViewById(R.id.head_cancel);//取消更换头像
-        TextView fromPhotoAlbum = (TextView) view.findViewById(R.id.from_photo_album);//从手机相册里选择头像图片
+        WindowManager manager = getWindowManager();
+        DisplayMetrics metrics = new DisplayMetrics();
+        manager.getDefaultDisplay().getMetrics(metrics);
+        int w = metrics.widthPixels;
+        int h = metrics.heightPixels;
 
+        camera = (TextView) view.findViewById(R.id.head_camera);//拍照获取头像
+        TextView headChangeCancel = (TextView) view.findViewById(R.id.head_cancel);//取消更换头像
+        fromPhotoAlbum = (TextView) view.findViewById(R.id.from_photo_album);//从手机相册里选择头像图片
         //设置拍照 启用摄像头/待编辑
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String filePath="";
-                if (ContextCompat.checkSelfPermission(PersonalInfo.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(PersonalInfo.this,new String []{Manifest.permission.CAMERA},1);
-                }else {
-                    Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE,null);
-                    filePath=getCacheDir().getPath()+File.separator+String.valueOf(System.currentTimeMillis())+"camera"+".png";
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(filePath)));
-                    startActivity(intent);
-                }
+                String filePath = "";
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE, null);
+                filePath = getCacheDir().getPath() + File.separator + String.valueOf(System.currentTimeMillis()) + "camera" + ".png";
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(filePath)));
+                startActivity(intent);
+
             }
         });
         //设置取消更换头像
         headChangeCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cameraDialog.dismiss();
+                cameraWindow.dismiss();
             }
         });
         //从相册里选择头像图片/待编辑
         fromPhotoAlbum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivity(intent);
+            }
+        });
+        cameraWindow = new PopupWindow(view, w, h);
+        cameraWindow.setOutsideTouchable(true);
+        cameraWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+
+            @Override
+            public void onDismiss() {
 
             }
         });
-        cameraDialog.setContentView(view);
-        cameraDialog.setCanceledOnTouchOutside(true);
-        cameraDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-                backGroundAlpha((float) 1);
-            }
-        });
-        cameraDialog.show();
+        cameraWindow.showAtLocation(parent_panel, Gravity.CENTER, 0, 0);
     }
 
     //更换性别
@@ -140,4 +144,5 @@ public class PersonalInfo extends Activity {
         popupWindow.setOutsideTouchable(true);
         popupWindow.showAsDropDown(personalized_edit_signature);
     }
+
 }

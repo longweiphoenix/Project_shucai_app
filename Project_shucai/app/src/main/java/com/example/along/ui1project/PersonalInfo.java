c7e8d24dccf5af9cd646a82a2fcaa3023ed9235b
longweifeng <longweifeng@outlook.com>
2016-11-28 09:52:11 +0800

    个人设置修改

--- Project_shucai/app/src/main/java/com/example/along/ui1project/PersonalInfo.java
+++ Project_shucai/app/src/main/java/com/example/along/ui1project/PersonalInfo.java
@@ -77,6 +77,7 @@ public class PersonalInfo extends Activity {
         sexual = (LinearLayout) findViewById(R.id.sexual);
         user_sex_next_arrow = (TextView) findViewById(R.id.user_sex_next_arrow);//性别后的箭头
         personalized_edit_signature = (TextView) findViewById(R.id.personalized_edit_signature);//个人签名
+        personalized_edit_signature.setText("新的个性签名，我好洗换！");
         personalized_edit_signature_next_arrow = (TextView) findViewById(R.id.personalized_edit_signature_next_arrow);//个人签名后的箭头
         info_head_portrait = (ImageView) findViewById(R.id.info_head_portrait);//用户头像
         info_head_change = (LinearLayout) findViewById(R.id.info_head_change);//更换用户头像
@@ -175,8 +176,8 @@ public class PersonalInfo extends Activity {
         sexual_female.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                sexual_female.setTextColor(getResources().getColor(R.color.title_font_color));
-                sexual_male.setTextColor(getResources().getColor(R.color.view_margin_line));
+                sexual_female.setTextColor(PersonalInfo.this.getResources().getColor(R.color.title_font_color));
+                sexual_male.setTextColor(PersonalInfo.this.getResources().getColor(R.color.view_margin_line));
             }
         });
         sexualChange = new PopupWindow(view, w, h / 2);

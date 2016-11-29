10815f7ccb08073aa0fb84cd7655b72feb694909
xingyuzhu-xy <443243855@qq.com>
2016-11-29 16:45:00 +0800

    zxy

--- Project_shucai/app/src/main/java/com/example/zxy/NameSetting.java
+++ Project_shucai/app/src/main/java/com/example/zxy/NameSetting.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.along.ui1project.PersonalInfo;
 import com.example.along.ui1project.R;
@@ -13,16 +15,24 @@ import com.example.along.ui1project.R;
  * Created by Administrator on 2016/10/23.
  */
 public class NameSetting extends Activity {
-    ImageView nameSettingArowImageView;
+    ImageView nameSettingArowImageView;//返回
+    TextView nameCommit;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_setting);
+        nameCommit= (TextView) findViewById(R.id.name_setting_commit);
         nameSettingArowImageView = (ImageView) findViewById(R.id.name_setting_arow);
         nameSettingArowImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(NameSetting.this, PersonalInfo.class);
                 startActivity(intent);
+            }
+        });
+        nameCommit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(NameSetting.this,"修改成功！",Toast.LENGTH_LONG).show();
             }
         });
     }

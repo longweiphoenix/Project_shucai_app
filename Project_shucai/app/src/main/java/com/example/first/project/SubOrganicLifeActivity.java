a947154231af787a78a6a4faeaffe3ee89cce75c
lijunming <you@example.com>
2016-11-30 14:37:52 +0800

    完善了查看和点赞功能

--- Project_shucai/app/src/main/java/com/example/first/project/SubOrganicLifeActivity.java
+++ Project_shucai/app/src/main/java/com/example/first/project/SubOrganicLifeActivity.java
@@ -4,17 +4,27 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.along.ui1project.R;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Administrator on 2016/11/28.
  */
 public class SubOrganicLifeActivity extends Activity {
+
+    ImageView back;
+    ImageView share;
+    ImageView enshrine;
+
     TextView title; //标题
     WebView webView;
     @Override
@@ -24,17 +34,46 @@ public class SubOrganicLifeActivity extends Activity {
 
         title = (TextView) findViewById(R.id.title);
         webView = (WebView) findViewById(R.id.web_view);
+        back = (ImageView) findViewById(R.id.arow);
+        share = (ImageView) findViewById(R.id.share);
+        enshrine  = (ImageView) findViewById(R.id.enshrine);
+
+        back.setOnClickListener(clickListener);
+        share.setOnClickListener(clickListener);
+        enshrine.setOnClickListener(clickListener);
+
 
         webView.setWebViewClient(new WebViewClient());//设置一个内置浏览器
         webView.getSettings().setJavaScriptEnabled(true); //支持JS脚本
         webView.addJavascriptInterface(new WebViewInterface(SubOrganicLifeActivity.this),"demo");
 
-
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        title.setText(bundle.getString("title"));
-        webView.loadUrl("https://zhidao.baidu.com/question/392590798609619205.html");
+        String str=bundle.getString("title");
+        title.setText(str);
+        try {
+            URL url = new URL("https://zhidao.baidu.com/search?word="+str);
+            webView.loadUrl(url.toString().trim());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
     }
+
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.arow:
+                    finish();
+                    break;
+                case R.id.share:  //分享
+                    break;
+                case R.id.enshrine: //收藏
+                    break;
+            }
+        }
+    };
 
     class WebViewInterface{
         Context context;
@@ -45,5 +84,11 @@ public class SubOrganicLifeActivity extends Activity {
         public void showToast(String name){
 
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
     }
 }

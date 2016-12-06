36490405b7c8fe9025c1ff2b38a1364c339d87c3
xingyuzhu-xy <443243855@qq.com>
2016-12-06 16:29:50 +0800

    no message

--- Project_shucai/app/src/main/java/com/example/zxy/NameSettingActivity.java
+++ Project_shucai/app/src/main/java/com/example/zxy/NameSettingActivity.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.along.ui1project.PersonalInfoActivity;
 import com.example.along.ui1project.R;
+import com.example.zxy.http.NameAccount;
+
+import java.net.MalformedURLException;
 
 /**
  * Created by Administrator on 2016/10/23.
@@ -17,9 +21,11 @@ import com.example.along.ui1project.R;
 public class NameSettingActivity extends Activity {
     ImageView nameSettingArowImageView;//返回
     TextView nameCommit;
+    EditText name;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_setting);
+        name= (EditText) findViewById(R.id.name_setting_textview);
         nameCommit= (TextView) findViewById(R.id.name_setting_commit);
         nameSettingArowImageView = (ImageView) findViewById(R.id.name_setting_arow);
         nameSettingArowImageView.setOnClickListener(new View.OnClickListener() {
@@ -36,4 +42,12 @@ public class NameSettingActivity extends Activity {
             }
         });
     }
+    /*public void commit(){
+        try {
+         *//*   NameAccount nameAccount=new NameAccount(name.getText().toString().trim());*//*
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }*/
 }

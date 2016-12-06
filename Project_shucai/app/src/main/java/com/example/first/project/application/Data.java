546111aed0752ec29d400cc407c2a469224b2ece
lijunming <you@example.com>
2016-12-06 16:24:19 +0800

    设置了全了变量

---
+++ Project_shucai/app/src/main/java/com/example/first/project/application/Data.java
@@ -0,0 +1,19 @@
+package com.example.first.project.application;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/12/0006.
+ */
+
+public class Data extends Application {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
